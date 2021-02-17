910d02e528015873110d52fa5db0255dc562b74e
Jonathan Molyneux <johnny.molyneux@gmail.com>
2021-02-17 16:49:43 +0000

    lost 3 days work, re added sliders!

--- coffida_frontend/components/search.js
+++ coffida_frontend/components/search.js
@@ -8,6 +8,7 @@ import {
   FlatList,
 } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import Slider from '@react-native-community/slider';
 import styles from './stylesheet';
 
 class Search extends Component {
@@ -26,24 +27,53 @@ class Search extends Component {
       lat: 0,
       long: 0,
       photoPath: '',
-      avgOverall: 0,
-      avgPrice: 0,
-      avgQuality: 0,
-      acvClean: 0,
-      shop_reviews: [],
+      minOverall: 0,
+      minPrice: 0,
+      minQuality: 0,
+      minClean: 0,
+
     };
   }
 
   handleInput = (input) => {
     this.setState({searchInput: input});
   };
+  handleOverall = (input) => {
+    this.setState({minOverall: input})
+  }
+  handlePrice = (input) => {
+    this.setState({minPrice: input})
+  }
+  handleQuality = (input) => {
+    this.setState({minQuality: input})
+  }
+  handleClean = (input) => {
+    this.setState({minClean: input})
+  }
 
   searchLocations = async () => {
+
     const token = await AsyncStorage.getItem('@session_token');
+    let url = 'http://10.0.2.2:3333/api/1.0.0/find?'
+
+    if(this.state.searchInput != ''){
+      url += 'q=' + this.state.searchInput + '&';
+    }
+    if(this.state.minOverall > 0){
+      url += 'overall_rating=' + this.state.minOverall + '&'
+    }
+    if(this.state.minPrice > 0){
+      url += 'price_rating=' + this.state.minPrice + '&'
+    }
+    if(this.state.minQuality > 0){
+      url += 'quality_rating=' + this.state.minQuality + '&'
+    }
+    if(this.state.minClean > 0){
+      url += 'clenliness_rating=' + this.state.minClean + '&'
+    }
 
     try {
-      return fetch(
-        'http://10.0.2.2:3333/api/1.0.0/find?q=' + this.state.searchInput,
+      return fetch(url,
         {
           method: 'GET',
           headers: {
@@ -85,6 +115,68 @@ class Search extends Component {
     this.props.navigation.navigate('SearchResult');
   };
 
+
+  overallSlider(){
+    return(
+      <Slider
+            style={{width: 200, height: 40}}
+            minimumValue={0}
+            maximumValue={5}
+            value={this.state.minOverall}
+            step={1}
+            minimumTrackTintColor="#52e37b"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="green"
+            onValueChange={(value) => this.handleOverall(value)}
+          />
+    );
+  }
+  priceSlider(){
+    return(
+      <Slider
+            style={{width: 200, height: 40}}
+            minimumValue={0}
+            maximumValue={5}
+            value={this.state.minPrice}
+            step={1}
+            minimumTrackTintColor="#52e37b"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="green"
+            onValueChange={(value) => this.handlePrice(value)}
+          />
+    );
+  }
+  qaulSlider(){
+    return(
+      <Slider
+            style={{width: 200, height: 40}}
+            minimumValue={0}
+            maximumValue={5}
+            value={this.state.minQuality}
+            step={1}
+            minimumTrackTintColor="#52e37b"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="green"
+            onValueChange={(value) => this.handleQuality(value)}
+          />
+    );
+  }
+  cleanSlider(){
+    return(
+      <Slider
+            style={{width: 200, height: 40}}
+            minimumValue={0}
+            maximumValue={5}
+            value={this.state.minClean}
+            step={1}
+            minimumTrackTintColor="#52e37b"
+            maximumTrackTintColor="#000000"
+            thumbTintColor="green"
+            onValueChange={(value) => this.handleClean(value)}
+          />
+    );
+  }
+
   render() {
     //const nav = this.props.navigation;
     if (this.state.hasSubmitted === false) {
@@ -103,6 +195,16 @@ class Search extends Component {
             onChangeText={this.handleInput}
             value={this.state.searchInput}
           />
+          
+          <Text>Minmum Overall Rating {this.state.minOverall}</Text>
+          {this.overallSlider()}
+          <Text>Minmum Price Rating {this.state.minPrice}</Text>
+          {this.priceSlider()}
+          <Text>Minmum Quality Rating {this.state.minQuality}</Text>
+          {this.qaulSlider()}
+          <Text>Minmum Cleanliness Rating {this.state.minClean}</Text>
+          {this.cleanSlider()}
+
 
           <TouchableOpacity
             style={styles.button}
